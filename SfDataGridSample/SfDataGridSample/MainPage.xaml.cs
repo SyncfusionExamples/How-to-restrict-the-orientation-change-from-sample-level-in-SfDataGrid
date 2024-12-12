6ee5b80b06ad89c80f60fea411ce9accd2679493
SethupathyD <sethupathy.d@syncfusion.com>
2024-12-12 14:34:18 +0530

    code updated.

---
+++ SfDataGridSample/SfDataGridSample/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Syncfusion.Maui.DataGrid;
+using Syncfusion.Maui.DataGrid.Exporting;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf.Grid;
+
+namespace SfDataGridSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
